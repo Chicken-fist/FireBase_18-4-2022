6e8ed4dd443d80d91434db91a7d0d191a79cee85
ADMIN <ndang1336@gmail.com>
2022-04-18 10:41:22 +0700

    18/4/2022

---
+++ app/src/main/java/com/example/firebase/Sign_in.java
@@ -0,0 +1,14 @@
+package com.example.firebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity);
+    }
+}
